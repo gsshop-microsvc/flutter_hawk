ecc6ed617032f5f3ba35585ac53821e4d544a336
jungjy <0nlyoung7@gmail.com>
2021-08-29 15:49:16 +0900

    Implement : delete

--- android/src/main/kotlin/com/gsshop/mobile/flutter/flutter_hawk/FlutterHawkPlugin.kt
+++ android/src/main/kotlin/com/gsshop/mobile/flutter/flutter_hawk/FlutterHawkPlugin.kt
@@ -10,6 +10,8 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.PluginRegistry.Registrar
 import com.orhanobut.hawk.Hawk
 
 /** FlutterHawkPlugin */
@@ -20,20 +22,26 @@ class FlutterHawkPlugin: FlutterPlugin, MethodCallHandler {
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
   private var mContext: Context? = null
+  private var registrar: Registrar? = null
 
   companion object {
-    private var registrar: PluginRegistry.Registrar? = null
-
-    private fun setRegistrar(_registrar: PluginRegistry.Registrar) {
-        registrar = _registrar
+    @JvmStatic
+    fun registerWith(registrar: Registrar) {
+      val instance = FlutterHawkPlugin()
+      instance.registrar = registrar
+      instance.onAttachedToEngine(registrar.context(), registrar.messenger())
     }
   }
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    mContext = flutterPluginBinding.applicationContext
+    onAttachedToEngine(flutterPluginBinding.getApplicationContext(), flutterPluginBinding.getBinaryMessenger());
+  }
+
+  private fun onAttachedToEngine(applicationContext: Context, binaryMessenger: BinaryMessenger) {
+    mContext = applicationContext
     Hawk.init(mContext).build()
 
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_hawk")
+    channel = MethodChannel(binaryMessenger, "flutter_hawk")
     channel.setMethodCallHandler(this)
   }
 
@@ -45,11 +53,27 @@ class FlutterHawkPlugin: FlutterPlugin, MethodCallHandler {
       try {
         if (key != null ){
           value = Hawk.get(key)
+        } else {
+
         }
       } catch(e: Exception) {
         print(e.localizedMessage)
       }
       result.success(value)
+    } else if (call.method == "delete") {
+      var key: String? = call.argument("key")
+
+      try {
+        if (key != null ){
+          Hawk.delete(key)
+        } else {
+          result.success(false)
+        }
+      } catch(e: Exception) {
+        print(e.localizedMessage)
+        result.success(false)
+      }
+      result.success(true)
     } else {
       result.notImplemented()
     }
